43ca912b177249632e62daff68d5cc8f1e62ca86
Rahul Katara <rahulkatara@Rahuls-MacBook-Air-2.local>
2025-08-09 17:37:32 +0530

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rahulkatara",
+    author_email="rahul1234katara@gmail.com",
+    packages=find_packages()
+)
