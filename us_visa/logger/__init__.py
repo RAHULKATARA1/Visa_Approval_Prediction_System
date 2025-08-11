ce049308ecae498604d8b6d0ca224204ad729dcd
Rahul Katara <rahul1234katara@gmail.com>
2025-08-11 21:08:57 +0530

    update

--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -1,17 +1,16 @@
 import logging
 import os
-
 from from_root import from_root
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-
 log_dir = 'logs'
 
+# Full logs path inside the project root
 logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
 
-os.makedirs(log_dir, exist_ok=True)
-
+# Make sure the folder exists
+os.makedirs(os.path.join(from_root(), log_dir), exist_ok=True)
 
 logging.basicConfig(
     filename=logs_path,
